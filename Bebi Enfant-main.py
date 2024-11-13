892f12c3bb8f9dbce36c6cd17c84057169bbe637
GregoireLorea <gregoire.lorea@gmail.com>
2024-11-13 18:58:07 +0100

    envoi

---
+++ Bebi Enfant-main.py
@@ -0,0 +1,18 @@
+from microbit import *
+import music
+
+def open():
+    music.play(music.JUMP_UP)
+    display.show(Image.DUCK)
+    sleep(1000)
+    display.scroll('Be:Bi Enfant', delay=60)
+
+
+open()
+while True
+
+    en fonction de son état d'éveil, rassurer bebe
+    en fonction de l'ampleur et de la durée de ses mouvements
+    if 
+    if 
+    if
---
+++ Bebi Parent-main (1).py
@@ -0,0 +1,17 @@
+from microbit import *
+import music
+
+def open():
+    music.play(music.JUMP_UP)
+    display.show(Image.HOUSE)
+    sleep(1000)
+    display.scroll('Be:Bi Parent', delay=60)
+
+open()
+while True
+
+    en fonction de l'état deveil du bb, afficher et prévenir parent
+    en fonction de l'ampleur et de la durée de mouvement bb, prévenir parent
+    if 
+    if 
+    if
